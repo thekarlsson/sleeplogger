bfeabd75a1a6724148d2d77d4435b1aecb54a99a
The Benevolent Father <robinedman@gmail.com>
2012-03-09 19:56:58 +0100

    A bit of refactoring, small changes in behaviour and buttons are now physically disabled when appropriate.

--- public/viewmodels/SleepViewModel.js
+++ public/viewmodels/SleepViewModel.js
@@ -1,54 +1,71 @@
 var SleepViewModel = function() {
-	var self = this;
+    var self = this;
+    
+    // Observables
+    self.times = ko.observableArray([]);
+    
+    // Other variables
+    var maxNumberOfTimes = 20,
+        minNumberOfTimes = 5,
+        incrDecrBy = 5;
+    
+    // Operations
+    self.updateTimes = function(data) {
+        self.times([]);
+        $.map($.parseJSON(data), function(item) {
+            var itemDatetime = new XDate(item['datetime']);
+            item['date'] = itemDatetime.toString('yyyy-MM-dd');
+            item['time'] = itemDatetime.toString('HH:mm');
+            delete item['datetime'];
+            self.times.push(item);
+        });
+    }
+    
+    self.getTimes = function(numberOfTimes) {
+        $.get(window.location.pathname + '/times' + '/' + numberOfTimes, function(data) {
+            self.updateTimes(data);
+        });
+    }
+    
+    self.sleepNow = function() {
+        $.post(window.location.pathname + '/sleepnow', {"data": ko.toJSON("Jag are en Irlandsk cowboy!")},
+            function(data){
+                self.updateTimes(data);
+            });
+    }
+
+	self.isDecrementable = function () {
+		if (self.times().length > minNumberOfTimes + incrDecrBy) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	};
 	
-	// Observables
-	self.times = ko.observableArray([]);
-	
-	// Other variables
-	var maxNumberOfTimes = 10 //default value
-	
-	// Operations
-	
-	self.updateTimes = function(data) {
-		self.times([]);
-		$.map($.parseJSON(data), function(item) {
-			var itemDatetime = new XDate(item['datetime']);
-			item['date'] = itemDatetime.toString('yyyy-MM-dd');
-			item['time'] = itemDatetime.toString('HH:mm');
-			delete item['datetime'];
-			self.times.push(item);
-		});
-	}
-	
-	self.getInit = function() {
-		$.get(window.location.pathname + '/times' + '/' + maxNumberOfTimes, function(data) {
-		self.updateTimes(data);
-		});
-	}
-	
-	self.sleepNow = function() {
-		$.post(window.location.pathname + '/sleepnow', {"data": ko.toJSON("Jag are en Irlandsk cowboy!")},
-			function(data){
-				self.updateTimes(data);
-			});
-	}
-	
-	self.moreTimes = function() {
-		if (maxNumberOfTimes < 20){
-			maxNumberOfTimes += 5
-			self.getInit()
+	self.isIncrementable = function() {
+		if (self.times().length < maxNumberOfTimes - incrDecrBy) {
+			return true;
+		}
+		else {
+			return false;
 		}
 	}
-	
-	self.lessTimes = function() {
-		if (maxNumberOfTimes > 5) {
-			maxNumberOfTimes -= 5
-			self.getInit()
-		}
-	}
-	
-	// Load initial values from server
-	self.getInit()
+    
+    self.moreTimes = function() {
+        if (self.isIncrementable()){
+            self.getTimes(self.times().length + incrDecrBy);
+        }
+    }
+    
+    self.lessTimes = function() {
+        if (self.isDecrementable()) {
+            self.getTimes(self.times().length - incrDecrBy);
+        }
+    }
+    
+    // Load initial values from server
+    self.getTimes()
 }
 
 // Activate
